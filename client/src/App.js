640a00cb118fc153ab4972b4d72df40b39a46ff3
YemiHa <yemi.haile07@gmail.com>
2023-03-07 00:26:55 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import About from "./Components/About/About";
+import Home from "./Pages/Home/Home";
+import Answer from "./Pages/Answers/Answer";
+import AskQuestion from "./Pages/AskQuestion/AskQuestion";
+import Questions from "./Pages/Questions/Questions";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+
+function App() {
+  return (
+  
+    <Router>
+        <Header /> {/* Asni */} {/* Yemi */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Jerry */}
+          <Route path="/Questions" element={<Questions />} /> {/* Jerry */}
+          <Route path="/Login" element={<Login />} /> {/* Hana */}
+          <Route path="/Signup" element={<Signup />} /> {/* Asre */}
+          <Route path="/About" element={<About />} /> {/* Kal */}
+          <Route path="/Answer" element={<Answer />} /> {/* Mame */}
+          <Route path="/AskQuestion" element={<AskQuestion />} /> {/* Eyu */}
+        </Routes>
+        <Footer /> {/* Elc */} {/* Sule */}
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from './evangadi-logo-home.png';
+import './Header.css';
+
+function Header() {
+    <div>
+<div className="container">
+    <img src={logo}alt=''/>
+    <Link to={"/"}>home</Link>
+    <Link to={"/how-it-works"}>how it works</Link>
+    <button className="btn btn-lg btn-primary my-3" type="submit">
+          SIGN IN
+        </button>
+</div>
+</div>
+}
+export default Header;
